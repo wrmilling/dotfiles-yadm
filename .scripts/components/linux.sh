a8582854b5f06f9e3933c7b750823c1ac55ca762
Winston R. Milling <Winston_R_Milling@homedepot.com>
2019-09-19 15:39:27 -0400

    Have the main setup script work alone
    It should install yadm and grab the repo, then call the sub-scripts

--- .scripts/components/linux.sh
+++ .scripts/components/linux.sh
@@ -1,19 +1,13 @@
 #!/usr/bin/env bash
 
-function print_header() {
-  echo "##############################"
-  echo $1
-  echo "##############################"
-}
+source ~/.scripts/components/common.sh
 
 print_header "Ensure system is up to date"
 grep -qxF 'deb http://deb.debian.org/debian buster-backports main contrib non-free' /etc/apt/sources.list || sudo echo 'deb http://deb.debian.org/debian buster-backports main contrib non-free' >> /etc/apt/sources.list
 sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get dist-upgrade -y && sudo apt-get install git curl -y
 
-print_header "Install yadm and clone the dotfiles repo"
-sudo apt-get -t stretch-backports install yadm -y
-yadm clone git@github.com:WRMilling/dotfiles.git
-yadm submodule update --init --recursive
+print_header "Install linux brew"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
 
 print_header "Install zsh and set as default shell"
 sudo apt-get install zsh
@@ -23,11 +17,3 @@ print_header "Install vim"
 sudo apt-get install vim
 ~/.vim/install.sh
 
-print_header "Install linux brew"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
-
-print_header "Setup git"
-~/.scripts/setupGit.sh
-
-print_header "Using yadm bootstrap to complete setup"
-yadm bootstrap
