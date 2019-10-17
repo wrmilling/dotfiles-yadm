3c9553be2d3653d26cccbb09ea85c7b72cf72803
Winston R. Milling <Winston_R_Milling@homedepot.com>
2019-10-17 11:50:13 -0400

    Change how we load nvm on OSX
    It is sloooooow.

--- .zsh/zshrc.d/100.setup-nvm.sh
+++ .zsh/zshrc.d/100.setup-nvm.sh
@@ -1,6 +1,8 @@
 if [[ "$OSTYPE" == "darwin"* ]]; then
   export NVM_DIR="$HOME/.nvm"
-  source $(brew --prefix nvm)/nvm.sh
+  [ -s "/usr/local/opt/nvm/etc/bash_completion" ] && . "/usr/local/opt/nvm/etc/bash_completion"
+  export PATH="$NVM_DIR/versions/node/v$(<$NVM_DIR/alias/default)/bin:$PATH"
+  alias nvm="unalias nvm; [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"; nvm $@"
 else
   export NVM_DIR="${XDG_CONFIG_HOME/:-$HOME/.}nvm"
   [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
